77cba7872ee5d868f9001f5210eafff2e6f01713
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-28 20:36:15 -0300

    criado class Book

---
+++ aula-2023-02-28/prj-biblioteca/src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    //attributes
+    public long isbn;
+    public String title;
+    public String author;
+    public double price;
+
+    //methods
+    public void registryLending(){}
+
+    public void registryDevolution(){}
+
+    public void printOut(){}
+}

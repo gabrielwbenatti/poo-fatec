d1621559ba8fbe101ecaaf3b485bb5e07b01e14e
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-28 22:08:54 -0300

    criado classe apolice teste

---
+++ aula-2023-02-28/prj-apolice/src/ApoliceTeste.java
@@ -0,0 +1,10 @@
+public class ApoliceTeste {
+    public static void main(String[] args) {
+        Apolice apolice = new Apolice();
+        apolice.setNomeSegurado("Gabriel Wisky Benatti");
+        apolice.setIdade(20);
+        apolice.setValorPremio(3650.55F);
+
+        apolice.imprimir();
+    }
+}

c8dcffd957237bc6181e6b6cd3aadbbe7d4034c2
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-28 22:05:06 -0300

    criado classe, getter e setters

---
+++ aula-2023-02-28/prj-apolice/src/Apolice.java
@@ -0,0 +1,29 @@
+public class Apolice {
+    private String nomeSegurado;
+    private int idade;
+    private float valorPremio;
+
+    public String getNomeSegurado() {
+        return nomeSegurado;
+    }
+
+    public void setNomeSegurado(String nomeSegurado) {
+        this.nomeSegurado = nomeSegurado;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getValorPremio() {
+        return valorPremio;
+    }
+
+    public void setValorPremio(float valorPremio) {
+        this.valorPremio = valorPremio;
+    }
+}

c5f6db76214a312e3eb4c0e2d8cfb429e469e90c
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-28 22:01:06 -0300

    criado getter e setter

--- aula-2023-02-28/prj-biblioteca/src/BibliotecaGerenciamento.java
+++ aula-2023-02-28/prj-biblioteca/src/BibliotecaGerenciamento.java
@@ -3,15 +3,15 @@ public class BibliotecaGerenciamento {
         Book book1 = new Book();
         Book book2 = new Book();
 
-        book1.isbn = 11;
-        book1.title="A Dama de Vermelho";
-        book1.author="Milionario e Jose Rico";
-        book1.price = 12.99;
+        book1.setIsbn(11);
+        book1.setTitle("A Dama de Vermelho");
+        book1.setAuthor("Milionario e Jose Rico");
+        book1.setPrice(12.99);
 
-        book2.isbn = 16;
-        book2.title="Branca de Neve";
-        book2.author="Maromo";
-        book2.price = 19.95;
+        book2.setIsbn(16);
+        book2.setTitle("Branca de Neve");
+        book2.setAuthor("Maromo");
+        book2.setPrice(19.95);
 
         // Registry lending
         book2.registryLending();
--- aula-2023-02-28/prj-biblioteca/src/Book.java
+++ aula-2023-02-28/prj-biblioteca/src/Book.java
@@ -1,11 +1,43 @@
 import java.time.LocalDateTime;
 
 public class Book {
+    public long getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     //attributes
-    public long isbn;
-    public String title;
-    public String author;
-    public double price;
+    private long isbn;
+    private String title;
+    private String author;
+    private double price;
+
 
     //methods
     public void registryLending() {

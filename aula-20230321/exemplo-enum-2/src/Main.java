ac21b4b990057a871c453d21ac862702ca30dfd5
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-03-21 20:36:40 -0300

    exemplo 2 de enum

--- aula-20230321/exemplo-enum-2/src/Main.java
+++ aula-20230321/exemplo-enum-2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Meses mes;
+        mes = Meses.jun;
+
+        System.out.println(mes);
     }
 }

c7245bbeaa9f6e0dfe8153f431437f0bbcadef8d
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-03-28 12:02:35 -0300

    add funcionalidades e renomeado projeto

--- desafios/jogo-21/src/Baralho.java
+++ desafios/jogo-21/src/Baralho.java
@@ -1,121 +1,133 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Baralho {
     private ArrayList<Carta> baralho;
+
+    public Baralho() {
+        baralho = new ArrayList<Carta>();
+    }
+
+    public void inicializarBaralho() {
+        if (this.baralho.isEmpty()) {
+            alimentarBaralho();
+        }
+        Collections.shuffle(this.baralho);
+    }
 
     private void alimentarBaralho() {
         Carta c1;
 
         //COPAS
-        c1 = new Carta("as","Copas",11);
+        c1 = new Carta("as", "Copas", 11);
         inserir(c1);
-        c1 = new Carta("dois","Copas",2);
+        c1 = new Carta("dois", "Copas", 2);
         inserir(c1);
-        c1 = new Carta("tres","Copas",3);
+        c1 = new Carta("tres", "Copas", 3);
         inserir(c1);
-        c1 = new Carta("quatro","Copas",4);
+        c1 = new Carta("quatro", "Copas", 4);
         inserir(c1);
-        c1 = new Carta("cinco","Copas",5);
+        c1 = new Carta("cinco", "Copas", 5);
         inserir(c1);
-        c1 = new Carta("seis","Copas",6);
+        c1 = new Carta("seis", "Copas", 6);
         inserir(c1);
-        c1 = new Carta("sete","Copas",7);
+        c1 = new Carta("sete", "Copas", 7);
         inserir(c1);
-        c1 = new Carta("oito","Copas",8);
+        c1 = new Carta("oito", "Copas", 8);
         inserir(c1);
-        c1 = new Carta("nove","Copas",9);
+        c1 = new Carta("nove", "Copas", 9);
         inserir(c1);
-        c1 = new Carta("dez","Copas",10);
+        c1 = new Carta("dez", "Copas", 10);
         inserir(c1);
-        c1 = new Carta("valete","Copas",10);
+        c1 = new Carta("valete", "Copas", 10);
         inserir(c1);
-        c1 = new Carta("dama","Copas",10);
+        c1 = new Carta("dama", "Copas", 10);
         inserir(c1);
-        c1 = new Carta("rei","Copas",10);
+        c1 = new Carta("rei", "Copas", 10);
         inserir(c1);
 
         //PAUS
-        c1 = new Carta("as","Paus",11);
+        c1 = new Carta("as", "Paus", 11);
         inserir(c1);
-        c1 = new Carta("dois","Paus",2);
+        c1 = new Carta("dois", "Paus", 2);
         inserir(c1);
-        c1 = new Carta("tres","Paus",3);
+        c1 = new Carta("tres", "Paus", 3);
         inserir(c1);
-        c1 = new Carta("quatro","Paus",4);
+        c1 = new Carta("quatro", "Paus", 4);
         inserir(c1);
-        c1 = new Carta("cinco","Paus",5);
+        c1 = new Carta("cinco", "Paus", 5);
         inserir(c1);
-        c1 = new Carta("seis","Paus",6);
+        c1 = new Carta("seis", "Paus", 6);
         inserir(c1);
-        c1 = new Carta("sete","Paus",7);
+        c1 = new Carta("sete", "Paus", 7);
         inserir(c1);
-        c1 = new Carta("oito","Paus",8);
+        c1 = new Carta("oito", "Paus", 8);
         inserir(c1);
-        c1 = new Carta("nove","Paus",9);
+        c1 = new Carta("nove", "Paus", 9);
         inserir(c1);
-        c1 = new Carta("dez","Paus",10);
+        c1 = new Carta("dez", "Paus", 10);
         inserir(c1);
-        c1 = new Carta("valete","Paus",10);
+        c1 = new Carta("valete", "Paus", 10);
         inserir(c1);
-        c1 = new Carta("dama","Paus",10);
+        c1 = new Carta("dama", "Paus", 10);
         inserir(c1);
-        c1 = new Carta("rei","Paus",10);
+        c1 = new Carta("rei", "Paus", 10);
         inserir(c1);
 
         //OURO
-        c1 = new Carta("as","Ouro",11);
+        c1 = new Carta("as", "Ouro", 11);
         inserir(c1);
-        c1 = new Carta("dois","Ouro",2);
+        c1 = new Carta("dois", "Ouro", 2);
         inserir(c1);
-        c1 = new Carta("tres","Ouro",3);
+        c1 = new Carta("tres", "Ouro", 3);
         inserir(c1);
-        c1 = new Carta("quatro","Ouro",4);
+        c1 = new Carta("quatro", "Ouro", 4);
         inserir(c1);
-        c1 = new Carta("cinco","Ouro",5);
+        c1 = new Carta("cinco", "Ouro", 5);
         inserir(c1);
-        c1 = new Carta("seis","Ouro",6);
+        c1 = new Carta("seis", "Ouro", 6);
         inserir(c1);
-        c1 = new Carta("sete","Ouro",7);
+        c1 = new Carta("sete", "Ouro", 7);
         inserir(c1);
-        c1 = new Carta("oito","Ouro",8);
+        c1 = new Carta("oito", "Ouro", 8);
         inserir(c1);
-        c1 = new Carta("nove","Ouro",9);
+        c1 = new Carta("nove", "Ouro", 9);
         inserir(c1);
-        c1 = new Carta("dez","Ouro",10);
+        c1 = new Carta("dez", "Ouro", 10);
         inserir(c1);
-        c1 = new Carta("valete","Ouro",10);
+        c1 = new Carta("valete", "Ouro", 10);
         inserir(c1);
-        c1 = new Carta("dama","Ouro",10);
+        c1 = new Carta("dama", "Ouro", 10);
         inserir(c1);
-        c1 = new Carta("rei","Ouro",10);
+        c1 = new Carta("rei", "Ouro", 10);
         inserir(c1);
 
         //ESPADAS
-        c1 = new Carta("as","Espadas",11);
+        c1 = new Carta("as", "Espadas", 11);
         inserir(c1);
-        c1 = new Carta("dois","Espadas",2);
+        c1 = new Carta("dois", "Espadas", 2);
         inserir(c1);
-        c1 = new Carta("tres","Espadas",3);
+        c1 = new Carta("tres", "Espadas", 3);
         inserir(c1);
-        c1 = new Carta("quatro","Espadas",4);
+        c1 = new Carta("quatro", "Espadas", 4);
         inserir(c1);
-        c1 = new Carta("cinco","Espadas",5);
+        c1 = new Carta("cinco", "Espadas", 5);
         inserir(c1);
-        c1 = new Carta("seis","Espadas",6);
+        c1 = new Carta("seis", "Espadas", 6);
         inserir(c1);
-        c1 = new Carta("sete","Espadas",7);
+        c1 = new Carta("sete", "Espadas", 7);
         inserir(c1);
-        c1 = new Carta("oito","Espadas",8);
+        c1 = new Carta("oito", "Espadas", 8);
         inserir(c1);
-        c1 = new Carta("nove","Espadas",9);
+        c1 = new Carta("nove", "Espadas", 9);
         inserir(c1);
-        c1 = new Carta("dez","Espadas",10);
+        c1 = new Carta("dez", "Espadas", 10);
         inserir(c1);
-        c1 = new Carta("valete","Espadas",10);
+        c1 = new Carta("valete", "Espadas", 10);
         inserir(c1);
-        c1 = new Carta("dama","Espadas",10);
+        c1 = new Carta("dama", "Espadas", 10);
         inserir(c1);
-        c1 = new Carta("rei","Espadas",10);
+        c1 = new Carta("rei", "Espadas", 10);
         inserir(c1);
 
     }
--- desafios/jogo-21/src/Jogo21.java
+++ desafios/jogo-21/src/Jogo21.java
@@ -1,5 +1,36 @@
-public class Jogo21 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Jogo21 {
+    private static Baralho baralho;
+    private static ArrayList<Jogador> jogadores;
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int op = 1;
+
+        Jogador jogador;
+        baralho = new Baralho();
+        jogadores = new ArrayList<Jogador>();
+        baralho.inicializarBaralho();
+
+        jogador = new Jogador();
+        jogador.setNome("Computador");
+        jogadores.add(jogador);
+
+        while (op >= 1) {
+            System.out.println(" --- JOGO DOS 21 PONTOS --- ");
+            System.out.printf(" 1) Adicionar novo jogador [%d jogador(es)]\n", jogadores.toArray().length - 1);
+            System.out.println(" 2) Iniciar jogo ");
+            System.out.println(" 0) Sair ");
+            op = Integer.parseInt(scan.nextLine());
+
+            if (op == 1) {
+                jogador = new Jogador();
+                System.out.println(" Digite o nome do jogador: ");
+                jogador.setNome(scan.nextLine());
+                jogadores.add(jogador);
+            }
+        }
     }
 }

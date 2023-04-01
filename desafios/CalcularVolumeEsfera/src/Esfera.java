172cec112d178eb479c3d54bfb0197a5bda852f3
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-03-31 22:02:58 -0300

    update esfera class

--- desafios/CalcularVolumeEsfera/src/Esfera.java
+++ desafios/CalcularVolumeEsfera/src/Esfera.java
@@ -2,7 +2,7 @@ public class Esfera {
     private double raio;
 
     public double getVolume(double raio) {
-        return (4 * Math.PI *(Math.pow(raio, 3))) / 3;
+        return (4.0 * Math.PI *(Math.pow(raio, 3))) / 3.0;
     }
 
     public double getRaio() {
